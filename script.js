5ed03972e6d99952c77924cbad797c6f4298c204
Mona <2829922e@student.gla.ac.uk>
2025-01-31 01:43:10 -0800

    toggle menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById('toggle');
+const menu = document.querySelector('.menu');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+}) 
