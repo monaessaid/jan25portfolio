4f0040ae5d60df96848dfeded6864ced29d1fcd6
Mona <2829922e@student.gla.ac.uk>
2025-02-01 16:16:10 -0800

    hero slideshow working

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ const menu = document.querySelector('.menu');
 
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
-}) 
+});
+
+var swiper = new Swiper('.hero-swiper', {
+    slidesPerView: 1,
+    effect: 'fade',
+    autoplay: {
+        delay: 6000,
+    },
+})
